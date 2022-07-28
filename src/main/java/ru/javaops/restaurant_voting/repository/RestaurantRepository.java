1b2dfea29bd9cf79dae25bea49e6616c696bc569
RadSham <radsham9@gmail.com>
2022-07-28 22:22:50 +0300

    refactor4

--- src/main/java/ru/javaops/restaurant_voting/repository/RestaurantRepository.java
+++ src/main/java/ru/javaops/restaurant_voting/repository/RestaurantRepository.java
@@ -1,8 +1,10 @@
 package ru.javaops.restaurant_voting.repository;
 
+import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 import ru.javaops.restaurant_voting.model.Restaurant;
 
+@Repository
 @Transactional(readOnly = true)
 public interface RestaurantRepository extends BaseRepository<Restaurant> {
 }
--- src/main/java/ru/javaops/restaurant_voting/web/restaurant/AdminRestaurantController.java
+++ src/main/java/ru/javaops/restaurant_voting/web/restaurant/AdminRestaurantController.java
@@ -25,8 +25,6 @@ import static ru.javaops.restaurant_voting.util.validation.ValidationUtil.checkN
 @RestController
 @RequestMapping(value = AdminRestaurantController.REST_URL, produces = MediaType.APPLICATION_JSON_VALUE)
 @Slf4j
-// TODO: cache only most requested data!
-@CacheConfig(cacheNames = "users")
 public class AdminRestaurantController extends AbstractRestaurantController {
     static final String REST_URL = "/api/admin/restaurants";
 
@@ -37,7 +35,6 @@ public class AdminRestaurantController extends AbstractRestaurantController {
     }
 
     @GetMapping
-    @Cacheable
     public List<Restaurant> getAll() {
         log.info("getAll");
         return super.getAll();
